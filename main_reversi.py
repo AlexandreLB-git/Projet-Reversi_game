c00a87bd6a101e420e87c37915c67913e3bfbb72
Alexandre Lebleu <105150173+AlexandreLB-git@users.noreply.github.com>
2025-11-16 23:37:51 +0100

    Add files via upload

---
+++ main_reversi.py
@@ -0,0 +1,166 @@
+from bitreversi import BitboardReversi
+import os
+import sys
+import importlib.util
+import multiprocessing as mp
+import itertools
+import copy
+from collections import defaultdict
+import logging
+os.makedirs("logs", exist_ok=True)
+
+# Nom du fichier de log
+log_file = os.path.join("logs", "tournament.log")
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.FileHandler(log_file, mode='w', encoding='utf-8'),  # fichier
+        logging.StreamHandler()  # console
+    ]
+)
+logger = logging.getLogger(__name__)
+AGENTS_DIR = "agents"
+NUM_MATCHES_EACH_SIDE = 5
+TIME_PER_MOVE = 2
+
+GAME = BitboardReversi()
+
+def load_agent(agent_dir):
+    agent_path = os.path.join(AGENTS_DIR, agent_dir)
+    agent_file = os.path.join(agent_path, "agent.py")
+
+    # Ajouter le répertoire de l'agent dans sys.path pour pouvoir importer ses sous-modules
+    sys.path.insert(0, agent_path)
+
+    try:
+        spec = importlib.util.spec_from_file_location(agent_dir, agent_file)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+    finally:
+        # Retirer le chemin pour éviter les conflits avec d'autres agents
+        sys.path.pop(0)
+
+    # Chercher la première classe qui commence par "Agent"
+    for attr in dir(module):
+        if attr.startswith("Agent"):
+            return  getattr(module, attr)
+
+    raise ValueError(f"Aucune classe d'agent trouvée dans {agent_file}")
+    
+
+def run_agent_move(agent, board, player, result_queue):
+    """Code exécuté dans un processus séparé pour respecter le timeout."""
+    try:
+        move = agent.play(player)
+        result_queue.put(move)
+    except Exception as e:
+        logger.error(e)
+        result_queue.put(None)
+
+def play_game(agent1_class, agent2_class,  time_limit=1.0):
+    GAME.reset()
+    board_init = GAME
+    agents = {
+        1: agent1_class(board_init.copy()),
+        -1: agent2_class(board_init.copy()),
+    }
+    board = board_init.copy()
+    player = 1
+
+    while not board.game_over():
+        agent = agents[player]
+        agent.board = board.copy()
+
+        legal_moves = board.valid_moves(player)
+        if not legal_moves:
+            player = -player
+            continue
+
+        result_queue = mp.Queue()
+        p = mp.Process(target=run_agent_move, args=(agent, board, player, result_queue))
+        p.start()
+        p.join(timeout=time_limit)
+
+        if p.is_alive():
+            p.terminate()
+            p.join()
+            logger.error(f"Too late for player {1 if player>0 else -1}")
+            move = None
+        else:
+            move = result_queue.get()
+
+        if move not in legal_moves:
+            logger.error(f"Illegal move for player {1 if player>0 else -1}")
+            move = None
+
+        if move is not None:
+            board.make_move(*move, player)
+        else:
+            return -player*100
+
+        player = -player
+
+    return board.score()
+
+# --- Tournoi Round-Robin ---
+def run_tournament():
+    agent_dirs = sorted(os.listdir(AGENTS_DIR))
+    agents = {name: load_agent(name) for name in agent_dirs}
+    scores = defaultdict(int)
+    results = defaultdict(lambda: {"wins": 0, "draws": 0, "losses": 0})
+    history = defaultdict(list)
+    agent_names = list(agents.keys())
+    pairs = list(itertools.combinations(agent_names, 2))
+    for a1, a2 in pairs:
+        for i in range(NUM_MATCHES_EACH_SIDE):
+            # Aller : A1 (noir), A2 (blanc)
+            logger.debug(f"Playing : {a1} vs {a2} match {i}")
+            winner = play_game(agents[a1], agents[a2],TIME_PER_MOVE)
+            if winner >0:
+                scores[a1] += 1
+                results[a1]["wins"] += 1
+                results[a2]["losses"] += 1
+            elif winner <0:
+                scores[a2] += 1
+                results[a2]["wins"] += 1
+                results[a1]["losses"] += 1
+            else:
+                scores[a1] += 0.5
+                scores[a2] += 0.5
+                results[a1]["draws"] += 1
+                results[a2]["draws"] += 1
+            history[a1].append((a1,a2,winner))
+            history[a2].append((a1,a2,winner))
+            # Retour : A2 (noir), A1 (blanc)
+            winner = play_game(agents[a2], agents[a1], TIME_PER_MOVE)
+            if winner >0 :
+                scores[a2] += 1
+                results[a2]["wins"] += 1
+                results[a1]["losses"] += 1
+            elif winner <0:
+                scores[a1] += 1
+                results[a1]["wins"] += 1
+                results[a2]["losses"] += 1
+            else:
+                scores[a1] += 0.5
+                scores[a2] += 0.5
+                results[a1]["draws"] += 1
+                results[a2]["draws"] += 1
+            history[a1].append((a2,a1,winner))
+            history[a2].append((a2,a1,winner))
+
+    logger.info("\n=== Results ===")
+    sorted_agents = sorted(agent_dirs, key=lambda name: scores[name], reverse=True)
+    for name in sorted_agents:
+        s = scores[name]
+        r = results[name]
+    logger.info(f"{name:20} | Score: {s:4.1f} | W: {r['wins']} D: {r['draws']} L: {r['losses']}")
+    logger.info("\n*** History \n***")
+    for name in sorted_agents:
+        moves_history = "|".join(f"{m1} vs {m2} : {score}" for (m1, m2, score) in history[name])
+        logger.info(f"{name} : {moves_history}")
+
+if __name__ == "__main__":
+    run_tournament()
---
+++ tme4.py
@@ -0,0 +1,142 @@
+import numpy as np
+import random
+import tme2 
+from tme2 import rollout
+
+class  AgentFlatMCTS:
+    def __init__(self,board,nb_simu=100):
+        self.board=board
+        self.nb_simu=nb_simu
+
+
+    def play(self, turn):
+        """
+        Choisit le meilleur coup à jouer pour 'turn' après nb_simu simulations.
+        """
+        game = self.board
+        legal_moves = game.valid_moves(turn)
+        if not legal_moves:
+            return (-1, -1)
+
+        best_move = None
+        best_ratio = -1.0
+
+        for (row, col) in legal_moves:
+            wins = 0
+
+            for _ in range(self.nb_simu):
+                # copie du jeu
+                g = game.copy()
+                g.make_move(row, col, turn)
+
+                # simule la suite de la partie au hasard
+                final = rollout(g, -turn,100) #100: nb_moves
+                score = final.score()
+
+                # si le joueur courant a gagné
+                if (turn == 1 and score > 0) or (turn == -1 and score < 0):
+                    wins += 1
+
+            ratio = wins / self.nb_simu
+            if ratio > best_ratio:
+                best_ratio = ratio
+                best_move = (row, col)
+
+        return best_move
+
+
+
+
+
+
+class MCTSNode:
+    def __init__(self, game, player, parent=None):
+        self.game = game.copy()
+        self.player = player
+        self.parent = parent
+        self.children = {}
+        self.config = game.board_to_int()
+        self.visits = 0
+        self.wins = 0
+        self.untried_moves = game.valid_moves(player)
+
+    def is_fully_expanded(self):
+        return len(self.untried_moves) == 0
+
+    def uct(self, child, K=1.4):
+        if child.visits == 0:
+            return float("inf")
+        return (child.wins / child.visits) + np.sqrt(K * np.log(self.visits + 1) / child.visits)
+
+    def best_move(self, K=1.4):
+        best_score = -float("inf")
+        best_move = None
+        best_child = None
+        for move, child in self.children.items():
+            score = self.uct(child, K)
+            if score > best_score:
+                best_score = score
+                best_move = move
+                best_child = child
+        return best_move, best_child
+
+
+class AgentMCTS:
+    def __init__(self, game, nb_simu=50, K=1.4):
+        self.game = game
+        self.nb_simu = nb_simu
+        self.K = K
+        self.nodes = {}
+
+    def play(self, player):
+        # récupérer ou créer le noeud racine
+        config = self.game.board_to_int()
+        if config not in self.nodes:
+            root = MCTSNode(self.game, player)
+            self.nodes[config] = root
+        else:
+            root = self.nodes[config]
+
+        # boucle principale
+        for _ in range(self.nb_simu):
+            node = root
+            game_copy = self.game.copy()
+            current_player = player
+
+            
+            while node.is_fully_expanded() and len(node.children) > 0 and not game_copy.game_over():
+                move, node = node.best_move(self.K)
+                game_copy.make_move(move[0], move[1], current_player)
+                current_player *= -1
+
+            
+            if not node.is_fully_expanded() and not game_copy.game_over():
+                move = random.choice(node.untried_moves)
+                node.untried_moves.remove(move)
+                game_copy.make_move(move[0], move[1], current_player)
+                current_player *= -1
+                child = MCTSNode(game_copy, current_player, parent=node)
+                node.children[move] = child
+                node = child
+
+            #Simulation 
+            final_game = rollout(game_copy, current_player, 100)
+            score = final_game.score()
+            if score > 0:
+                result = 1
+            elif score < 0:
+                result = -1
+            else:
+                result = 0
+
+
+            while node is not None:
+                node.visits += 1
+                if result == node.player:
+                    node.wins += 1
+                node = node.parent
+
+        if not root.children:
+            return (-1, -1)
+        best_move = max(root.children.items(), key=lambda item: item[1].visits)[0]
+        return best_move
